79ae411520f0676357d348ddcfddc339047792c3
Austin Parker <austincparker@gmail.com>
2021-07-21 15:07:00 -0500

    move object to array

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
-const austin = {
+const group3 = [{
     id: 1,
     name: "Austin",
     email: "parkercaustin@gmail.com",
     color: "green",
     unique: "son lives in England",
     image: "https://avatars.githubusercontent.com/u/70224936?v=4"
-}
+},
 
-const gabriel = {
+{
     id: 2,
     name: "Gabriel",
     email: "gabrielsmith98@gmail.com",
     color: "green",
     unique: "likes video games",
     image: "https://avatars.githubusercontent.com/u/86996271?v=4"
-}
+},
 
-const ruby = {
+{
     id: 3,
     name: "Ruby",
     email: "rubiblr@gmail",
     color: "yellow",
     unique: "likes to travel",
     image: "https://avatars.githubusercontent.com/u/86809246?v=4"
-}
+},
 
-const luke = {
+{
     id: 4,
     name: "Luke",
     email: "mallen1813@gmail.com",
     color: "yellow",
     unique: "trying for fourth child",
     image: "https://avatars.githubusercontent.com/u/74151134?v=4"
-}
+}];
 
-console.log(austin.image);
+console.log(group3[0]);
